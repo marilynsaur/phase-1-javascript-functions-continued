805376251699074ad33bc4ad8825cf85668d3ecb
Marilyn Wren <Marilynwren64@yahoo.com>
2022-02-04 13:41:02 -0500

    commit tree b24e05c684ca222a36f7c4d64cbac74153924480

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`;
+}
+saturdayFun(undefined);
+saturdayFun("bathe my dog");
+
+
+function mondayWork(plans = "go to the office") {
+    return `This Monday, I will ${plans}.`;
+}
+mondayWork(undefined);
+mondayWork("work from home.");
+
+function wrapAdjective(greeting,msg = "You are" ) {
+    return function (name,cutie ="!") {
+        return `${msg} ${greeting}${name}${greeting}${cutie}`;
+};
+
+}
+wrapAdjective ("*")("a hard worker");
+wrapAdjective ("||")("a dedicated programmer");
